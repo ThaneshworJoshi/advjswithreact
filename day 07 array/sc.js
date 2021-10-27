670b21632f8a383c3c64baaf35834b3dc9c5d84b
tyagisen <tyagisen@gmail.com>
2021-10-27 09:13:58 +0545

    Adding anonymous for each function

--- day 07 array/sc.js
+++ day 07 array/sc.js
@@ -1,5 +1,5 @@
 // object literal syntax
-// date, math, reular expressions, array, function, object
+// date, math, reular expressions, array, function, object are object
 // all js values, except primitives, are objects
 // string, number, boolean, null, undefined are primitive dt
 // method are the actions that can be performed on object.
@@ -12,18 +12,19 @@ let myobj = {
     age : 22,
     height: "5' 6\"",
 };
+console.log("accessing object")
 console.log(myobj.height);
 
 
 delete myobj.height;
 console.log(myobj)
-// creating objects in object literal
 
+// creating objects in object literal
 myobj.weight = 50;
 console.log(myobj)
 myobj.firstName = 'manabi'
 
-
+// creating object in another way
 let obj1 = new Object();
 
 obj1.firstName = 'Moti';
@@ -32,6 +33,7 @@ console.log(obj1);
 
 
 // accesing Object
+console.log("Accessing first name from object")
 console.log(myobj['firstName']);
 console.log(myobj.firstName);
 
@@ -39,10 +41,15 @@ console.log(myobj.firstName);
 // updating object
 obj1.age=17;
 
+// deleting object
+console.log("Deleting age")
+delete obj1.age;
+console.log(obj1)
 console.log(obj1['age'])
 // console.log(obj1.age)
 
 // looping objects
+console.log("looping through the objects")
 for (let key in myobj){
     console.log(myobj[key])
     console.log(key)
@@ -66,10 +73,18 @@ for (let key in myobj){
 
 let arr = [1,'ram', 'hari', 3, 5,1,'ram', 'hari', 3, 5];
 console.log(arr[arr.length-1]);
-
+console.log("Looping array")
 for(let x=0; x<arr.length; x++){
     console.log(arr[x])
 }
+
+
+// function without name is anonymous function
+console.log("Looping array using anonymous function. It can't be called as other functions")
+arr.forEach(function(item){
+    console.log(item)
+});
+
 
 
 console.log(typeof(arr))
