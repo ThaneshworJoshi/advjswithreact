378cdb80663b377c3c1ed35dc9105d73897823f8
tyagisen <tyagisen@gmail.com>
2021-11-21 20:11:34 +0545

    array and its method

--- day 15 array methods/obj.js
+++ day 15 array methods/obj.js
@@ -3,5 +3,16 @@
 let arr = ['apple', 11, {age:23}, function(){return 'tyagi';}];
 console.log(arr[2].age)
 console.log(arr[3]())
+// replacing array
+arr[0] = "grapes";
+console.log(arr)
 
+// queue: queue supports push and shift
+let myarr = ['tyagi', 1 ,'samir', 'zebra']
+myarr.push("horse")
+console.log(myarr)
+myarr.shift()
 
+// stack: push, pop
+myarr.push("my")
+myarr.pop()
