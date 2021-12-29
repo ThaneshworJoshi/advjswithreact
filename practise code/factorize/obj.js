945290dbf0989bd5fcc1f1cbe778df5a8a79ffac
tyagisen <tyagisen@gmail.com>
2021-12-29 15:24:04 +0545

    Factorize of Number

---
+++ practise code/factorize/obj.js
@@ -0,0 +1,11 @@
+// factorizing number
+function factorize(num){
+  let result=1;
+  for(let i = 1; i<=num; i++){
+    result *=i
+    console.log('r')
+  }
+  return result
+
+}
+console.log(factorize(5))
